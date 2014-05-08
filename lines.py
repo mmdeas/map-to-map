a6de6efe79cbeba3b906136dcc24a6fba2f47705
Miguel Martinez de Aguirre <miguel@aguirredevelopment.co.uk>
2014-05-08 15:25:30 +0100

    Starting to work - still has issues with thick lines and transparency.

--- lines.py
+++ lines.py
@@ -1,13 +1,22 @@
 from Queue import Queue
 from itertools import product
 
-MIN_LINE_LENGTH = 0
+MIN_LINE_LENGTH = 5
+
+# TODO: deal with 45 degree angles
 
 
-def straight_lines(im):
+def straight_lines(im, visual=False):
     # threshold at 90%
+    im = im.convert("RGBA")
+    if visual:
+        im.show()
     im = im.convert("L")
+    if visual:
+        im.show()
     im = im.point(_threshold)
+    if visual:
+        im.show()
     pa = im.load()
     expanded = set()
     queue = Queue()
@@ -15,20 +24,24 @@ def straight_lines(im):
     # iterate over all pixels
     for pixel in product(xrange(im.size[0]),
                          xrange(im.size[1])):
-        # add unexpanded white pixels to queue for each direction
+        # add unexpanded white pixels to queue for all directions
         if not _is_black(pa[pixel]):
-            for direction in range(4):
-                if (pixel, direction) not in expanded:
-                    qo = QueueObject(pixel, None, None, direction, 0, pa)
-                    queue.put(qo)
+            if (pixel) not in expanded:
+                qo = QueueObject(pixel, None, None, None, 0, pa)
+                queue.put(qo)
         while not queue.empty():
             node = queue.get()
-            expanded.add((node.pixel, direction))
+            if node.pixel in expanded:
+                continue
+            expanded.add((node.pixel))
             children = node.expand()
+            exps = 0
             for child in children:
-                if (child.pixel, direction) not in expanded:
+                if (child.pixel) not in expanded:
                     queue.put(child)
-            if len(children) == 0 and node.length > MIN_LINE_LENGTH:
+                else:
+                    exps += 1
+            if len(children) == exps and node.length > MIN_LINE_LENGTH:
                 lines.append((node.root, node))
     lines_by_se = {}
     for line in lines:
@@ -44,60 +57,57 @@ def straight_lines(im):
 
 class QueueObject(object):
     """Class to keep information about the line search."""
-    # direction: 0 -> UP, 1 -> RIGHT, 2 -> DOWN, 3 -> LEFT
-    def __init__(self, pixel, parent, root, direction, allowed, length, pa):
+    dir_enable = {
+        (-1, -1): set([(0, -1), (-1, -1), (-1, 0)]),
+        (-1, 0): set([(-1, -1), (-1, 0), (-1, 1)]),
+        (-1, 1): set([(-1, 0), (-1, 1), (0, 1)]),
+        (0, 1): set([(-1, 1), (0, 1), (1, 1)]),
+        (1, 1): set([(0, 1), (1, 1), (1, 0)]),
+        (1, 0): set([(1, 1), (1, 0), (1, -1)]),
+        (1, -1): set([(1, 0), (1, -1), (0, -1)]),
+        (0, -1): set([(1, -1), (0, -1), (-1, -1)])
+    }
+
+    def __init__(self, pixel, parent, root, directions, length, pa):
         super(QueueObject, self).__init__()
         self.pixel = pixel
         self.parent = parent
         if root is None:
             root = self
         self.root = root
-        self.direction = direction
-        if allowed is None:
-            allowed = [1, 1]
+        if directions is None:
+            directions = set([d for d in product([-1, 0, 1], [-1, 0, 1])
+                             if d != (0, 0)])
+        self.directions = directions
         self.length = length
         self.pa = pa
+        if root.pixel == (33, 57):
+          print self, parent
 
     def expand(self):
-        # pixels is iter of poss child pixels
-        # UP
-        if self.direction == 0:
-            pixels = [(x, self.pixel[1] - 1)
-                      for x in range(self.pixel[0] - 1, self.pixel[0] + 2)]
-            allowed = []
-        # RIGHT
-        elif self.direction == 1:
-            pixels = list(product([self.pixel[0] + 1],
-                                  range(self.pixel[1] - 1, self.pixel[1] + 2)))
-        # DOWN
-        elif self.direction == 2:
-            pixels = list(product(range(self.pixel[0] - 1, self.pixel[0] + 2),
-                                  [self.pixel[1] + 1]))
-        # LEFT
-        elif self.direction == 3:
-            pixels = list(product([self.pixel[0] - 1],
-                                  range(self.pixel[1] - 1, self.pixel[1] + 2)))
-
         children = []
-        for pixel in pixels:
+        for direction in self.directions:
+            new = tuple(map(lambda x, y: x+y, self.pixel, direction))
             try:
-                if not _is_black(self.pa[pixel]):
-                    children.append(QueueObject(pixel, self, self.root,
-                                    self.direction, self.length + 1, self.pa))
+                if not _is_black(self.pa[new]):
+                    new_dirs = self.directions.intersection(self.dir_enable[direction])
+                    children.append(QueueObject(new, self, self.root,
+                                    new_dirs, self.length + 1, self.pa))
             except IndexError:
                 pass
         return children
 
     def __repr__(self):
-        return ''.join(('<', str(self.pixel), ', ', str(self.direction), '>'))
+        dir_str = 'all' if len(self.directions) == 8 else str(self.directions)
+        return ''.join(('<', repr(self.pixel), ', ', repr(self.length), ', ', dir_str, '>'))
 
 
 def _is_black(colour):
     return colour == 0
 
 
-def _threshold(pixel, percentage=90):
-        return (pixel > 255 * percentage / 100) * 255
+def _threshold(image, percentage=90):
+    return (pixel > 255 * percentage / 100) * 255
 
 
 if __name__ == '__main__':
@@ -105,11 +115,8 @@ if __name__ == '__main__':
     from PIL import Image
     for arg in sys.argv[1:]:
         im = Image.open(arg)
-        im = im.convert('RGBA')
-        im.show()
         lines = straight_lines(im)
         print arg, 'lines:', len(lines)
-        print arg, lines
         im = Image.new("RGB", im.size)
         pa = im.load()
         for line in lines:
