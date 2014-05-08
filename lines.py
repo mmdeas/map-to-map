8384420902fa5fd4b01bbfdef1917f214da1b7ea
Miguel Martinez de Aguirre <miguel@aguirredevelopment.co.uk>
2014-05-08 17:38:49 +0100

    Added Zhang-Suen line-thinning.

--- lines.py
+++ lines.py
@@ -8,13 +8,10 @@ MIN_LINE_LENGTH = 5
 
 def straight_lines(im, visual=False):
     # threshold at 90%
-    im = im.convert("RGBA")
     if visual:
+        im = im.convert("RGBA")
         im.show()
-    im = im.convert("L")
-    if visual:
-        im.show()
-    im = im.point(_threshold)
+    im = _threshold(im)
     if visual:
         im.show()
     pa = im.load()
@@ -81,8 +78,6 @@ class QueueObject(object):
         self.directions = directions
         self.length = length
         self.pa = pa
-        if root.pixel == (33, 57):
-          print self, parent
 
     def expand(self):
         children = []
@@ -90,24 +85,90 @@ class QueueObject(object):
             new = tuple(map(lambda x, y: x+y, self.pixel, direction))
             try:
                 if not _is_black(self.pa[new]):
-                    new_dirs = self.directions.intersection(self.dir_enable[direction])
+                    new_dirs = self.directions.intersection(
+                        self.dir_enable[direction])
                     children.append(QueueObject(new, self, self.root,
                                     new_dirs, self.length + 1, self.pa))
             except IndexError:
                 pass
+        self.children = children
         return children
 
     def __repr__(self):
         dir_str = 'all' if len(self.directions) == 8 else str(self.directions)
-        return ''.join(('<', repr(self.pixel), ', ', repr(self.length), ', ', dir_str, '>'))
+        return ''.join(('<',
+                        repr(self.pixel), ', ',
+                        repr(self.length), ', ',
+                        dir_str, '>'))
 
 
 def _is_black(colour):
     return colour == 0
 
 
-def _threshold(image, percentage=90):
-    return (pixel > 255 * percentage / 100) * 255
+def _threshold(image, threshold=0.9):
+    # remove transparency
+    image = image.convert("RGBA")
+    pa = image.load()
+    for p in product(xrange(image.size[0]), xrange(image.size[1])):
+        colour = pa[p]
+        if colour[-1] < 255 or sum(colour[:-1]) < 255 * 3 * threshold:
+            pa[p] = (0, 0, 0, 255)
+        else:
+            pa[p] = (255, 255, 255, 255)
+    image = image.convert("1")
+    image.show()
+    pa = image.load()
+    zhangSuen(pa, image.size[0], image.size[1])
+    image.show()
+    return image
+
+
+# neighbours, transitions and zhangSuen modified from
+# http://rosettacode.org/wiki/Zhang-Suen_thinning_algorithm
+def neighbours(x, y, image):
+    '''Return 8-neighbours of point p1 of picture, in order'''
+    i = image
+    x1, y1, x_1, y_1 = x+1, y-1, x-1, y+1
+    return [i[y1, x],  i[y1, x1],   i[y, x1],  i[y_1, x1],  # P2,P3,P4,P5
+            i[y_1, x], i[y_1, x_1], i[y, x_1], i[y1, x_1]]  # P6,P7,P8,P9
+
+
+def transitions(neighbours):
+    n = neighbours + neighbours[0:1]    # P2, ... P9, P2
+    return sum((n1, n2) == (0, 255) for n1, n2 in zip(n, n[1:]))
+
+
+def zhangSuen(image, width, height):
+    changing1 = changing2 = [(-1, -1)]
+    while changing1 or changing2:
+        # Step 1
+        changing1 = []
+        for y in range(1, height - 1):
+            for x in range(1, width - 1):
+                P2, P3, P4, P5, P6, P7, P8, P9 = n = neighbours(x, y, image)
+                if (image[y, x] == 255 and          # (Condition 0)
+                        P4 * P6 * P8 == 0 and       # Condition 4
+                        P2 * P4 * P6 == 0 and       # Condition 3
+                        transitions(n) == 1 and     # Condition 2
+                        2*255 <= sum(n) <= 6*255):  # Condition 1
+                    changing1.append((x, y))
+        for x, y in changing1:
+            image[y, x] = 0
+        # Step 2
+        changing2 = []
+        for y in range(1, height - 1):
+            for x in range(1, width - 1):
+                P2, P3, P4, P5, P6, P7, P8, P9 = n = neighbours(x, y, image)
+                if (image[y, x] == 255 and          # (Condition 0)
+                        P2 * P6 * P8 == 0 and       # Condition 4
+                        P2 * P4 * P8 == 0 and       # Condition 3
+                        transitions(n) == 1 and     # Condition 2
+                        2*255 <= sum(n) <= 6*255):  # Condition 1
+                    changing2.append((x, y))
+        for x, y in changing2:
+            image[y, x] = 0
+    return image
 
 
 if __name__ == '__main__':
@@ -115,7 +176,7 @@ if __name__ == '__main__':
     from PIL import Image
     for arg in sys.argv[1:]:
         im = Image.open(arg)
-        lines = straight_lines(im)
+        lines = straight_lines(im, True)
         print arg, 'lines:', len(lines)
         im = Image.new("RGB", im.size)
         pa = im.load()
